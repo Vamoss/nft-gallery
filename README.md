b7489a3acaf43929ce4fc6ed501cd2566981fdaf
Vamoss <quissama@gmail.com>
2024-03-24 14:33:27 -0300

    next.js

---
+++ README.md
@@ -0,0 +1,7 @@
+First, run the development server:
+
+```bash
+npm run dev
+```
+
+Open [http://localhost:80](http://localhost:80) with your browser to see the result.
---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Vamoss - NFT Gallery",
+  description: "Vamoss is an artist-programmer who uses code as raw material to create generative art projects.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
